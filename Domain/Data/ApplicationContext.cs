86042718e8117f6028b1ee9cfc7df9fb79e89c4a
Adriano Supeleto <adriano.supeleto.17@gmail.com>
2023-04-04 14:21:13 -0300

    Criação e configuração da classe de Contexto

---
+++ Domain/Data/ApplicationContext.cs
@@ -0,0 +1,23 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.Extensions.Logging;
+
+namespace Curso.Domain.Data
+{
+    public class ApplicationContext : DbContext
+    {
+        public DbSet<Departamento> Departamentos { get; set; }
+        public DbSet<Funcionario> Funcionarios { get; set; }
+
+        protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+        {
+            const string strConnection = "Data source=ADRIANOSUPELETO\\ADRIANO; Initial Catalog=DominandoEFCore; Integrated Security=true; polling=true;";
+            optionsBuilder.UseSqlServer(strConnection)
+            .EnableSensitiveDataLogging()
+            .LogTo(Console.WriteLine, LogLevel.Information);
+        }
+    }
+}
